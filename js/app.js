7475882270fd39839e7258488f288af193c1be95
Maria <maanraca01@gmail.com>
2025-05-09 22:57:03 -0500

    Se agrega funcion para mostrar alertas en el formulario

--- js/app.js
+++ js/app.js
@@ -5,10 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputEmail = document.querySelector("#email");
     const inputSubject = document.querySelector("#subject" );
     const inputMessage = document.querySelector("#message");
+    const contactForm = document.querySelector("#contactForm");
     
     //fn Validateinputs
     const validateInputs = ((e) => {
-        console.log(e.target.value);
+        console.log(e.target.parentElement);
+        if(e.target.value.trim() === ""){
+            showalert(` ${e.target.id} field is required`, e.target.parentElement);
+        }else {
+            console.log("Si hay algo")
+        }
     })
    
     //Events
@@ -16,4 +22,15 @@ document.addEventListener("DOMContentLoaded", () => {
     inputEmail.addEventListener("blur", validateInputs);
     inputSubject.addEventListener("blur", validateInputs);
     inputMessage.addEventListener("blur", validateInputs);
+
+    const showalert = ((message, referencia) => {
+        //Alert on Html
+        const error = document.createElement("P");
+        error.textContent = message;
+        error.classList.add("error-message");
+        
+        //Inject the error into the form
+        referencia.appendChild(error);
+
+    })
 })
