47db7caca989e23d0cdc328e4e95508135fa796f
Maria <maanraca01@gmail.com>
2025-05-09 20:01:24 -0500

    Se agrega el proyecto

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Seleccionando input
+    const inputName = document.querySelector("#name");
+    const inputEmail = document.querySelector("#email");
+    const inputSubject = document.querySelector("#subject" );
+    const inputMessage = document.querySelector("#message");
+    console.log(inputMessage);
+})
